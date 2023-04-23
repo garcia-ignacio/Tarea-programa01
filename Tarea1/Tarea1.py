03cb2330051aeab9c5f9810eef1b09ee13466a35
Titocf23 <99340138+Titocf23@users.noreply.github.com>
2023-04-22 18:49:38 -0600

    Update Tarea1.py

--- Tarea1/Tarea1.py
+++ Tarea1/Tarea1.py
@@ -1,45 +1,33 @@
 import random
 
-def InsertaRAnimales():
+def agregar_animales():
+    nombre_zoo = input("Ingrese un nombre para su zoológico: ")
+    try:
+        with open('animales.txt', 'r') as f:
+            animales = f.read().splitlines()
+    except FileNotFoundError:
+        print("No se pudo encontrar el archivo de animales.")
+        animales = []
     
-    with open("animales.txt", "r") as f:
-        nombres_animales = [line.strip() for line in f]
-
-    cantidad_animales = random.choice(range(1, len(nombres_animales)))
-    animales_zoologico = random.sample(nombres_animales, cantidad_animales)
-
-    respuesta = input("¿Desea cargar datos de un archivo binario previo? (s/n)")
-
-    if respuesta == "s":
-        ruta_archivo = input("Ingrese la ruta del archivo binario: ")
-        # Cargar datos en una matriz y crear una lista simple con los nombres de los animales
-    else:
-        animales_zoologico = []
-        
-    print(f"El zoológico '{nombre_zoologico}' tiene los siguientes animales:")
-    for animal in animales_zoologico:
+    num_animales = input("Ingrese la cantidad de animales que desea agregar: ")
+    try:
+        num_animales = int(num_animales)
+    except ValueError:
+        print("Ingrese un número válido.")
+        return
+    
+    if num_animales > len(animales):
+        print(f"No hay suficientes animales en la lista. Hay {len(animales)} animales disponibles.")
+        return
+    
+    random.shuffle(animales)
+    animales_zoo = animales[:num_animales]
+    
+    print(f"Los siguientes animales han sido agregados a {nombre_zoo}:")
+    for animal in animales_zoo:
         print(animal)
+    
+    return animales_zoo
 
-    import random
 
-    nombre_zoologico = input("Ingrese un nombre para su zoológico: ")
-
-    with open("nombres_animales.txt", "r") as f:
-        nombres_animales = [line.strip() for line in f]
-
-    cantidad_animales = random.choice(range(1, len(nombres_animales)))
-    animales_zoologico = random.sample(nombres_animales, cantidad_animales)
-
-    respuesta = input("¿Desea cargar datos de un archivo binario previo? (s/n)")
-
-    if respuesta == "s":
-        ruta_archivo = input("Ingrese la ruta del archivo binario: ")
-        # Cargar datos en una matriz y crear una lista simple con los nombres de los animales
-    else:
-        animales_zoologico = []
-
-    print(f"El zoológico '{nombre_zoologico}' tiene los siguientes animales:")
-    for animal in animales_zoologico:
-        print(animal)
-
-InsertaRAnimales()
+agregar_animales()
